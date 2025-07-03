d68eea3369d66473e3df33046d1612f53ecd4cd5
c-j-johnston <cameron.johnston@fullfact.org>
2025-07-03 16:27:32 +0100

    feat: add claim-type-detector to repo

---
+++ src/harmful_claim_finder/claim_type_detector/__init__.py
@@ -0,0 +1,5 @@
+from . import production_inference, claim_typer
+
+ClaimTyper = production_inference.ClaimTyper
+create_claim_typer = production_inference.create_claim_typer
+ClaimTypeResult = claim_typer.ClaimTypeResult
