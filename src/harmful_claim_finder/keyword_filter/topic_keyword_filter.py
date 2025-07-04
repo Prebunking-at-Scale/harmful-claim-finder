a8c80c91ebfc75fbc036049f713489f097adcacc
Edward Dearden <ed.dearden@fullfact.org>
2025-07-04 11:13:08 +0100

    chore: ran isort

--- src/harmful_claim_finder/keyword_filter/topic_keyword_filter.py
+++ src/harmful_claim_finder/keyword_filter/topic_keyword_filter.py
@@ -2,10 +2,7 @@ import logging
 import re
 import traceback
 
-from harmful_claim_finder.keyword_filter.prompts import (
-    FIX_JSON,
-    TOPIC_PROMPT,
-)
+from harmful_claim_finder.keyword_filter.prompts import FIX_JSON, TOPIC_PROMPT
 from harmful_claim_finder.utils.gemini import GeminiError, run_prompt
 from harmful_claim_finder.utils.models import ParsingError, TopicDetectionError
 from harmful_claim_finder.utils.parsing import ParsedType, parse_model_json_output
