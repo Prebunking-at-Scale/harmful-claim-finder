11640574851a0dc5578d59db94cec2eb202effcf
c-j-johnston <cameron.johnston@fullfact.org>
2025-07-04 15:23:03 +0100

    chore: remove exports from __init__

--- src/harmful_claim_finder/claim_type_detector/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import production_inference, claim_typer
-
-ClaimTyper = production_inference.ClaimTyper
-create_claim_typer = production_inference.create_claim_typer
-ClaimTypeResult = claim_typer.ClaimTypeResult
