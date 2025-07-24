5b1e24de4bd0cf1fce1372a2baa7a11d78936962
Edward Dearden <32845136+dearden@users.noreply.github.com>
2025-07-24 12:28:42 +0100

    feat: added keywords for video claim extraction (#16)

--- src/harmful_claim_finder/video_inference.py
+++ src/harmful_claim_finder/video_inference.py
@@ -10,7 +10,10 @@ from harmful_claim_finder.utils.models import VideoClaims
 
 
 async def get_claims(
-    video_id: UUID, video_uri: str, country_codes: list[str]
+    video_id: UUID,
+    video_uri: str,
+    keywords: dict[str, list[str]],
+    country_codes: list[str],
 ) -> list[VideoClaims]:
     """
     Retrieve claims from a video directly.
@@ -21,7 +24,14 @@ async def get_claims(
         video_uri: str
             A URI to a video in a Google Cloud Bucket.
             The file should be an mp4.
-
+        keywords (dict[str, list[str]]):
+            A {topic: keywords} dictionary containing the kw for each topic. E.g.
+            ```python
+            {
+                "crime": ["police", "robbers"],
+                "health": ["doctor", "hospital"],
+            }
+            ```
         country_codes (list[str]):
             A list of 3-letter ISO country codes for the current sentences.
             e.g. `["GBR", "USA"]`
@@ -30,7 +40,9 @@ async def get_claims(
         list[VideoClaims]
             A list of claims, marked up with scores.
     """
-    claims: list[VideoClaims] = await extract_claims_from_video(video_id, video_uri)
+    claims: list[VideoClaims] = await extract_claims_from_video(
+        video_id, video_uri, keywords
+    )
     pastel = CheckworthyClaimDetector(countries=country_codes)
     claims_text = [claim.claim for claim in claims]
     scores = await pastel.score_sentences(claims_text, max_attempts=2)
