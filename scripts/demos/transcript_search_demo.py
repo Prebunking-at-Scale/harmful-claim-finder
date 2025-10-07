30a3d6fa4b2c275b1026e6e8d533525fedab66ad
Edward Dearden <32845136+dearden@users.noreply.github.com>
2025-10-07 15:13:35 +0100

    feat: added transcript search as an inference method (#23)

---
+++ scripts/demos/transcript_search_demo.py
@@ -0,0 +1,63 @@
+import asyncio
+import json
+from pathlib import Path
+from typing import TypedDict
+from uuid import UUID
+
+from harmful_claim_finder.transcript_search import get_claims
+from harmful_claim_finder.utils.models import TranscriptSentence
+
+
+class TranscriptFragment(TypedDict):
+    text: str
+    start: float
+    duration: float
+
+
+transcripts = [
+    "data/example_transcripts/9V1U_hnxEjo.json",
+    "data/example_transcripts/8PqlzOVfciQ.json",
+    "data/example_transcripts/noNa3WUGpC8.json",
+    "data/example_transcripts/kMWlOcjBdMc.json",
+    "data/example_transcripts/uIFSvnB1WlQ.json",
+]
+
+video_id = UUID("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+
+def find_checkworthy_claims() -> None:
+    output = {}
+    kw = {
+        "vaccines": ["covid-19", "vaxxer", "vaccine"],
+        "miracle_cures": ["miracle cure", "magic herbs", "traditional medicine"],
+    }
+    output = {}
+    for transcript_path in transcripts:
+        try:
+            transcript: list[TranscriptFragment] = json.loads(
+                Path(transcript_path).read_text()
+            )
+            sentences = [
+                TranscriptSentence(
+                    video_id=video_id,
+                    source="",
+                    text=fragment["text"],
+                    start_time_s=fragment["start"],
+                )
+                for fragment in transcript
+            ]
+            claims = asyncio.run(get_claims(kw, sentences))
+            print(f"Found {len(claims)} claims in transcript {transcript_path}")
+            jsonable = [claim.model_dump(mode="json") for claim in claims]
+            output[transcript_path] = jsonable
+        except Exception as exc:
+            print(f"Something went wrong with {transcript_path}: {repr(exc)}")
+            continue
+
+    Path("output_transcript_search_demo.json").write_text(
+        json.dumps(output, indent=4, ensure_ascii=False)
+    )
+
+
+if __name__ == "__main__":
+    find_checkworthy_claims()
---
+++ src/harmful_claim_finder/transcript_search.py
@@ -0,0 +1,63 @@
+"""
+Searches a video transcript for claims.
+Does not make the assumption that a sentence is a claim.
+Claims can span more than one sentence, or be a span within a sentence.
+"""
+
+from harmful_claim_finder.claim_extraction import extract_claims_from_transcript
+from harmful_claim_finder.pastel.inference import CheckworthyClaimDetector
+from harmful_claim_finder.utils.models import (
+    CheckworthyError,
+    ClaimExtractionError,
+    PastelError,
+    TranscriptSentence,
+    VideoClaims,
+)
+
+
+async def get_claims(
+    keywords: dict[str, list[str]],
+    transcript: list[TranscriptSentence],
+) -> list[VideoClaims]:
+    """
+    Retrieve claims from a video transcript.
+    Claims can be more than one sentence, or part of a sentence.
+
+    Args:
+        keywords (dict[str, list[str]]):
+            A {topic: keywords} dictionary containing the kw for each topic. E.g.
+            ```python
+            {
+                "crime": ["police", "robbers"],
+                "health": ["doctor", "hospital"],
+            }
+            ```
+        transcript (list[TranscriptSentence]):
+            The transcript you want to search for claims.
+
+    Returns:
+        list[VideoClaims]
+            A list of claims, marked up with scores.
+
+    Raises:
+        CheckworthyError:
+            If something goes wrong during claim extraction or
+            pastel, the CheckworthyError will say what went wrong.
+    """
+    try:
+        claims: list[VideoClaims] = await extract_claims_from_transcript(
+            transcript=transcript, keywords=keywords, max_attempts=2
+        )
+        pastel = CheckworthyClaimDetector()
+        claims_text = [claim.claim for claim in claims]
+        scores = await pastel.score_sentences(claims_text, max_attempts=2)
+
+        for claim, score in zip(claims, scores):
+            claim.metadata = (
+                {**claim.metadata, "score": score}
+                if claim.metadata
+                else {"score": score}
+            )
+        return claims
+    except (ClaimExtractionError, PastelError) as exc:
+        raise CheckworthyError from exc
