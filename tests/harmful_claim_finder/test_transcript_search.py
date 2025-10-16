0d0674e9feaa64d2266083ea2e231a07719e8213
c-j-johnston <cameron.johnston@fullfact.org>
2025-10-16 16:29:16 +0100

    tests: update search and inference tests

--- tests/harmful_claim_finder/test_transcript_search.py
+++ tests/harmful_claim_finder/test_transcript_search.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from harmful_claim_finder.pastel.inference import CheckworthyClaimDetector
 from harmful_claim_finder.transcript_search import get_claims
 from harmful_claim_finder.utils.models import VideoClaims
+from harmful_claim_finder.pastel.pastel import ScoresAndAnswers
 
 fake_id = UUID("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
@@ -33,19 +34,19 @@ scored_claims = [
         video_id=fake_id,
         claim="claim 1",
         start_time_s=0,
-        metadata={"quote": "quote 1", "score": 0.9},
+        metadata={"quote": "quote 1", "score": 0.9, "answers": {"q": 0.1}},
     ),
     VideoClaims(
         video_id=fake_id,
         claim="claim 2",
         start_time_s=1,
-        metadata={"quote": "quote 2", "score": 0.2},
+        metadata={"quote": "quote 2", "score": 0.2, "answers": {"q": 0.2}},
     ),
     VideoClaims(
         video_id=fake_id,
         claim="claim 3",
         start_time_s=2,
-        metadata={"quote": "quote 3", "score": 0},
+        metadata={"quote": "quote 3", "score": 0, "answers": {"q": 0.3}},
     ),
 ]
 
@@ -55,7 +56,11 @@ scored_claims = [
 async def test_output_format(mock_extract_claims, mock_pastel):
     mock_extract_claims.return_value = unscored_claims
     mock_pastel_class = Mock(CheckworthyClaimDetector)
-    mock_pastel_class.score_sentences.return_value = [0.9, 0.2, 0]
+    mock_pastel_class.score_sentences.return_value = {
+        "claim 1": ScoresAndAnswers(score=0.9, answers={"q": 0.1}),
+        "claim 2": ScoresAndAnswers(score=0.2, answers={"q": 0.2}),
+        "claim 3": ScoresAndAnswers(score=0, answers={"q": 0.3}),
+    }
     mock_pastel.return_value = mock_pastel_class
     kw = {"topic": ["keyword"]}
     output = await get_claims(kw, [])
